61fdda67d70d13b960e0df42d7f76cc4a8e8d03e
foomoon <daniel.j.claxton@gmail.com>
2023-11-11 09:23:28 -0600

    update warning

--- src/iotWebApp.h
+++ src/iotWebApp.h
@@ -20,11 +20,11 @@
 #include <ArduinoJson.h>
 #ifndef WEBPAGE_H
 #include "index.h" // Include the webpage "index.h" header
+#warning "No WEBPAGE found Using default HTML"
 #else
 // Use the externally defined version of HTML
 extern const uint32_t HTML_SIZE;
 extern const uint8_t HTML[] PROGMEM;
-#warning "Using external HTML"
 #endif
 
 typedef void (*FuncType)(uint8_t *, size_t);
